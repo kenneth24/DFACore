17a1dc2aa4aab8a863dc67c06db4d18f6640b1c2
krstloiflash <krstloflash@gmail.com>
2022-02-21 04:58:26 +0800

    available all dates if LRA

---
+++ DFACore/Models/DTO/ScheduleDates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFACore.Models.DTO
+{
+    public class ScheduleDates
+    {
+        public string title { get; set; }
+        public DateTime start { get; set; }
+        public string color { get; set; }
+    }
+}
